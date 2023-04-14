72942127aa98fc0f5d219c1e525c65219f550e08
bishoy gamal <126079142+bisho2023@users.noreply.github.com>
2023-04-14 02:11:52 +0200

    login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -10,28 +10,6 @@ import {
 } from "firebase/auth";
 export const Login = () => {
   const navigate = useNavigate();
-
-  // const [email, setEmail]=useState('');
-  // const [password, setPassword]=useState('');
-
-  // const [errorMsg, setErrorMsg]=useState('');
-  // const [successMsg, setSuccessMsg]=useState('');
-
-  // const handleLogin=(e)=>{
-  //     e.preventDefault();
-  //      console.log(email, password);
-  //    signInWithEmailAndPassword(auth,email,password).then(()=>{
-  //         setSuccessMsg('Login Successfull. You will now automatically get redirected to Home page');
-  //         setEmail('');
-  //         setPassword('');
-  //         setErrorMsg('');
-  //         setTimeout(()=>{
-  //             setSuccessMsg('');
-  //             navigate('/');
-  //         },3000)
-  //     }).catch(error=>setErrorMsg(error.message));
-  // }
-
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
@@ -48,36 +26,6 @@ export const Login = () => {
     navigate("/signup");
   };
   return (
-    // <div className='container'>
-    //     <br></br>
-    //     <br></br>
-    //     <h1>Login</h1>
-    //     <hr></hr>
-    //     {successMsg&&<>
-    //         <div className='success-msg'>{successMsg}</div>
-    //         <br></br>
-    //     </>}
-    //     <form className='form-group' autoComplete="off"
-    //     onSubmit={handleLogin}>
-    //         <label>Email</label>
-    //         <input type="email" className='form-control' required
-    //         onChange={(e)=>setEmail(e.target.value)} value={email}></input>
-    //         <br></br>
-    //         <label>Password</label>
-    //         <input type="password" className='form-control' required
-    //         onChange={(e)=>setPassword(e.target.value)} value={password}></input>
-    //         <br></br>
-    //         <div className='btn-box'>
-    //             <span>Don't have an account SignUp
-    //             <Link to="signup" className='link'> Here</Link></span>
-    //             <button type="submit" className='btn btn-success btn-md'>LOGIN</button>
-    //         </div>
-    //     </form>
-    //     {errorMsg&&<>
-    //         <br></br>
-    //         <div className='error-msg'>{errorMsg}</div>
-    //     </>}
-    // </div>
     <div className="login">
       <Link to="/">
         <img className="login-logo" src={Logo} alt="logo-img" />
