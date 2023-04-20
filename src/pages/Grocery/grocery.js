4bdc669fd338b7748f4fcca583af0f3e3123331e
Ahmed El-Ged <121124311+ahmedelsayed1996@users.noreply.github.com>
2023-04-20 05:52:43 +0200

    update

--- src/pages/Grocery/grocery.js
+++ src/pages/Grocery/grocery.js
@@ -11,12 +11,18 @@ import { db } from "../../firebase";
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 import cookies from 'js-cookie';
+import ReactStars from "react-rating-stars-component";
 
 
 
 
 
 const Grocery = () => {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+    alert(`the rating is ${newRating}`)
+
+  };
 
   
    const currentLanguageCode = cookies.get('i18next') || 'en'
@@ -115,6 +121,23 @@ const Grocery = () => {
 
 
                   {/* <h3>Rate : {prd.rating.rate}</h3> */}
+                  <p><ReactStars
+                             index={index}
+                             count={5}
+                             onChange={ratingChanged}
+                             value={index+1}
+                           size={24}
+                           isHalf={true} 
+                         activeColor="#ffd700"
+                  /></p><p><ReactStars
+                  index={index}
+                  count={5}
+                  onChange={ratingChanged}
+                  value={index+1}
+                size={24}
+                isHalf={true} 
+              activeColor="#ffd700"
+       /></p>
                   <button
                     style={{
                       fontSize: "14px",
--- src/pages/Mobile/Mobile.js
+++ src/pages/Mobile/Mobile.js
@@ -8,8 +8,14 @@ import { db } from '../../firebase';
 import { useTranslation } from 'react-i18next';
 import i18next from 'i18next';
 import cookies from 'js-cookie';
+import ReactStars from "react-rating-stars-component";
 
 const Mobile = () => {
+  const ratingChanged = (newRating) => {
+    console.log(newRating);
+    alert(`the rating is ${newRating}`)
+
+  };
      //language
      const currentLanguageCode = cookies.get('i18next') || 'en'
      const { t } = useTranslation();
@@ -100,6 +106,15 @@ const Mobile = () => {
                   <h3>{t("price")} {prd.price}</h3>
 
                   {/* <h3>Rate : {prd.rating.rate}</h3> */}
+                  <p><ReactStars
+                             index={index}
+                             count={5}
+                             onChange={ratingChanged}
+                             value={index+1}
+                           size={24}
+                           isHalf={true} 
+                         activeColor="#ffd700"
+                  /></p>
                   <button
                     style={{
                       fontSize: "14px",
