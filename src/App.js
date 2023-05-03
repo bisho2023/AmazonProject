752f6f8ddc6709cdfbbdede4efedca614b06f863
Ahmed El-Ged <121124311+ahmedelsayed1996@users.noreply.github.com>
2023-05-03 23:20:27 +0200

    update

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ import Tv from "./pages/TV/tv";
 import Videos from "./pages/Videos/videos";
 import cards from "./pages/cards/cards";
 import Cards from "./pages/cards/cards";
+import Help from "./pages/help/help";
 import Sidebar from "./Components/sidepar/sidebar";
 import Search from "./pages/search/search";
 import { Signup } from "./pages/signup/signup";
@@ -26,6 +27,7 @@ import { auth } from "./firebase";
 import { useEffect } from "react";
 import { useAuth } from "./context/GlobalProvider";
 import CheckOut from "./pages/order/CheckOut";
+import PageNotFound from "./pages/pagenotfound/pageNotFound"
 
 
 
